5ca9e64d1c3a8129966f58073207850c47ffbffb
Ignacio Mardonez <ignaciomardonezrivera@gmail.com>
2024-09-29 02:07:33 -0300

    Descripción de los cambios

---
+++ src/data/products.ts
@@ -0,0 +1,46 @@
+// products.ts
+export const PRODUCTS = [
+    {
+      name: "Pallet Tipo Estandar",
+      image: "/img/wallpaper.jpg",
+      description: "Pallet 1000 x 1200 cm.",
+      category: "Pallet",
+    },
+    {
+      name: "Pallet Tipo Euro",
+      image: "/img/wallpaper.jpg",
+      description: "Pallet 800 x 1200 cm.",
+      category: "Pallet",
+    },
+    {
+      name: "Pallet Tipo Yugo",
+      image: "/img/wallpaper.jpg",
+      description: "Pallet 1000 x 1200 cm.",
+      category: "Pallet",
+    },
+    {
+      name: "Pallet Personalizado",
+      image: "/img/wallpaper.jpg",
+      description: "????????????",
+      category: "Pallet",
+    },
+    {
+      name: "Pallet Materialero",
+      image: "/img/wallpaper.jpg",
+      description: "Pallet 1000 x 1200 cm.",
+      category: "Pallet",
+    },
+    {
+      name: "Bolsa 2.5 kg",
+      image: "/img/xtend_2_5kg.jpg",
+      description: "Bolsa de 2.5 kg para exportación de frutas y verduras.",
+      category: "Bolsa",
+    },
+    {
+      name: "Bolsa 5 kg",
+      image: "/img/xtend_5kg.jpg",
+      description: "Bolsa de 5 kg para exportación de frutas y verduras.",
+      category: "Bolsa",
+    },
+  ];
+  
---
+++ src/types/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    name: string;
+    image: string;
+    description: string;
+    category: string;
+  }
+  
